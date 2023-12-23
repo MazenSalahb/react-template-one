22848eae390ee1675c150fe4c00f2ee8342918ee
Mazen Salah <101013388+MazenSalahb@users.noreply.github.com>
2023-12-23 11:27:35 +0200

    v1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,34 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import logo from "../assets/logo.png";
 
 export default function Header() {
   const [isShowing, setIsShowing] = useState(false);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (linksRef.current && !linksRef.current.contains(event.target)) {
+        setIsShowing(false);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [linksRef]);
 
   return (
     <div className="header">
       <div className="container">
         <img className="logo" src={logo} alt="logo" />
-        <div className="links" onClick={() => setIsShowing(!isShowing)}>
+        <div
+          className="links"
+          onClick={() => setIsShowing(!isShowing)}
+          ref={linksRef}
+        >
           <span className="icon">
             <span></span>
             <span></span>
